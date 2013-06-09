909c1175927db3de200f640dd38a8bf106a34ebb
DrGlobox <locap25@gmail.com>
2013-06-09 22:06:01 +0200

    Rename function

--- placeFigure.pl
+++ placeFigure.pl
@@ -5,16 +5,16 @@
 
 
 
-%rotation_figure(Points, Origine, Angle, Points_tournes)
+%rotate_figure(Points, Origine, Angle, Points_tournes)
 %   avec Points liste de point et Angle entier en radian et Points_tournes
-rotation_figure([], _, _,[]):-!.
-rotation_figure([Point|Reste], Origine, Angle,[Point_tourne|Retour]):-
-    rotation_figure(Reste,Origine,Angle,Retour),
-    rotation_point(Point,Origine,Angle,Point_tourne).
+rotate_figure([], _, _,[]):-!.
+rotate_figure([Point|Reste], Origine, Angle,[Point_tourne|Retour]):-
+    rotate_figure(Reste,Origine,Angle,Retour),
+    rotate_point(Point,Origine,Angle,Point_tourne).
 
-%rotation_point(Point, Origine,Angle,Point_tourne)
+%rotate_point(Point, Origine,Angle,Point_tourne)
 %   rotation du point Point d'angle Angle autour du centre Origin
-rotation_point([Xp,Yp],[Xo,Yo],Angle,[Xr,Yr]):-
+rotate_point([Xp,Yp],[Xo,Yo],Angle,[Xr,Yr]):-
     % tools:distance([Xp,Yp],[Xo,Yo],D),
     CosAngle is cos(Angle), SinAngle is sin(Angle),
     Xcos is ((Xp-Xo)*CosAngle),
@@ -25,7 +25,7 @@ rotation_point([Xp,Yp],[Xo,Yo],Angle,[Xr,Yr]):-
     Yrr is  Xsin - Ycos + Yo,Yr is round(Yrr).
 
 
-angleDegre(Radian, Degre):-
+getAngleDegre(Radian, Degre):-
     Degre is (180 * (Radian / pi)).
 
 find_angle([[Ptx1,Pty1],[Ptx2,Pty2]],[[Ptx3,Pty3],[Ptx4,Pty4]],Angle):-
@@ -46,11 +46,11 @@ find_angle([[Ptx1,Pty1],[Ptx2,Pty2]],[[Ptx3,Pty3],[Ptx4,Pty4]],Angle):-
 
 
 
-%translation_figure(+Figure,+Vecteur,-NouvelleFigure)
+%translate_figure(+Figure,+Vecteur,-NouvelleFigure)
 %   retourne les nouvelles coordonnées de la figure tranlaté par le vecteur
-translation_figure([],_,[]).
-translation_figure([[Pox,Poy]|Reste],[Ax,Ay],[[Ptx,Pty]|Resultat]):-
-    translation_figure(Reste,[Ax,Ay],Resultat),
+translate_figure([],_,[]).
+translate_figure([[Pox,Poy]|Reste],[Ax,Ay],[[Ptx,Pty]|Resultat]):-
+    translate_figure(Reste,[Ax,Ay],Resultat),
     Ptx is Pox + Ax, Pty is Poy + Ay.
 
 polaire([X,Y],[Ox,Y],Teta,D):-
@@ -101,66 +101,66 @@ symetrie_figure([[Px,Py]|Reste],[[PAx,PAy],[PBx,PBy]],[[Pnx,Pny]|Resultat]):-
 
 %la méthode permet de tester toutes les possibilités de position pour une figure
 placeFigure(Figure,Dessins,ReturnFigure):-
-    iter_dessin(Figure,Dessins,Placement),
+    test_placement_dessins_exact_match(Figure,Dessins,Placement),
     positionne(Figure,Placement,NewFigure),
     test_all_placement(NewFigure,Dessins,Placement,ReturnFigure).
 
 
-test_all_placement(Figure,Dessins,_,Figure):-
-    test_dessins(Figure,Dessins),!.
-test_all_placement(Figure,Dessins,[_,[[Pfx1,Pfy1],[Pfx2,Pfy2]]],NewFigure):-
-    symetrie_figure(Figure,[[Pfx1,Pfy1],[Pfx2,Pfy2]],NewFigure),
-    test_dessins(NewFigure,Dessins),!.
-test_all_placement(_,_,_,_):-fail.
 
+
+
+%test_placement_dessins_exact_match permet d'iteré tous les figures qui compose le dessin
+test_placement_dessins_exact_match(_,[],_):-fail.
+test_placement_dessins_exact_match(Figure,[Dessin|_],Placement):- 
+    tools:aretes_figure(Dessin,AretesDessin), 
+    test_aretes_dessin_exact_match(Figure,AretesDessin,Placement).
+test_placement_dessins_exact_match(Figure,[_|Reste],Placement):- 
+    test_placement_dessins_exact_match(Figure,Reste,Placement).
+
+%test_aretes_dessin_exact_match permet pour chaque dessin d'itèré toutes les aretes 
+test_aretes_dessin_exact_match(_,[],_):-fail.
+test_aretes_dessin_exact_match(Figure,[Arete|_],Placement):-
+    tools:aretes_figure(Figure,AretesFigure),
+    test_figure_arete_dessin_exacte_match(AretesFigure,Arete,Placement).
+test_aretes_dessin_exact_match(Figure,[_|Reste],Placement):-
+    test_aretes_dessin_exact_match(Figure,Reste,Placement).
+
+%test_figure_arete_dessin_exacte_match permet pour chaque arete du dessin d'itèré toutes les aretes de la figure à tester
+test_figure_arete_dessin_exacte_match([],_,_):-fail.
+test_figure_arete_dessin_exacte_match([_|ResteFigure],AreteDessin,Placement):-
+    test_figure_arete_dessin_exacte_match(ResteFigure,AreteDessin,Placement).
+test_figure_arete_dessin_exacte_match([[Pf1,Pf2]|_],[Pd1,Pd2],[[Pf1,Pf2],[Pd1,Pd2]]):-  
+    distance(Pf1,Pf2,Df), distance(Pd1,Pd2,Dd), Df == Dd.
 
 
 
 
 positionne(PointFigure,[[[Pox1,Poy1],[Pox2,Poy2]],[[Pfx1,Pfy1],[Pfx2,Pfy2]]],NewFigure2):-
     Ax is Pfx1 - Pox1, Ay is Pfy1 - Poy1,
-    translation_figure(PointFigure,[Ax,Ay],NewFigure),
+    translate_figure(PointFigure,[Ax,Ay],NewFigure),
     find_angle([[Pox1,Poy1],[Pox2,Poy2]],[[Pfx1,Pfy1],[Pfx2,Pfy2]],Angle),
-    rotation_figure(NewFigure,[Pfx1,Pfy1],Angle,NewFigure2).
+    rotate_figure(NewFigure,[Pfx1,Pfy1],Angle,NewFigure2).
 
-test_dessins(_,[]):-!.
-test_dessins(Figure,[Dessin|Reste]):-
-    test_dessins(Figure,Reste),
-    test_point_figure(Figure,Dessin),!.
-test_dessins(_,_):-fail.
 
-test_point_figure([],_):-!.
-test_point_figure([Point|Rest],Dessin):-
-    test_point_figure(Rest,Dessin),
+
+test_all_placement(Figure,Dessins,_,Figure):-
+    test_placement_dessins(Figure,Dessins),!.
+test_all_placement(Figure,Dessins,[_,[[Pfx1,Pfy1],[Pfx2,Pfy2]]],NewFigure):-
+    symetrie_figure(Figure,[[Pfx1,Pfy1],[Pfx2,Pfy2]],NewFigure),
+    test_placement_dessins(NewFigure,Dessins),!.
+test_all_placement(_,_,_,_):-fail.
+
+test_placement_dessins(_,[]):-!.
+test_placement_dessins(Figure,[Dessin|Reste]):-
+    test_placement_dessins(Figure,Reste),
+    test_placement_figure_dessin(Figure,Dessin),!.
+test_placement_dessins(_,_):-fail.
+
+test_placement_figure_dessin([],_):-!.
+test_placement_figure_dessin([Point|Rest],Dessin):-
+    test_placement_figure_dessin(Rest,Dessin),
     pointIsInFigure:pointIsInFigure(Point,Dessin,Result),
     Result == 1,!.
-test_point_figure(_,_):-fail.
-
-
-
-
-
-%iter_dessin permet d'iteré tous les figures qui compose le dessin
-iter_dessin(_,[],_):-fail.
-iter_dessin(Figure,[Dessin|_],Placement):- 
-    tools:aretes_figure(Dessin,AretesDessin), 
-    iter_aretes_dessin(Figure,AretesDessin,Placement).
-iter_dessin(Figure,[_|Reste],Placement):- 
-    iter_dessin(Figure,Reste,Placement).
-
-%iter_aretes_dessin permet pour chaque dessin d'itèré toutes les aretes 
-iter_aretes_dessin(_,[],_):-fail.
-iter_aretes_dessin(Figure,[Arete|_],Placement):-
-    tools:aretes_figure(Figure,AretesFigure),
-    iter_figure(AretesFigure,Arete,Placement).
-iter_aretes_dessin(Figure,[_|Reste],Placement):-
-    iter_aretes_dessin(Figure,Reste,Placement).
-
-%iter_figure permet pour chaque arete du dessin d'itèré toutes les aretes de la figure à tester
-iter_figure([],_,_):-fail.
-iter_figure([_|ResteFigure],AreteDessin,Placement):-
-    iter_figure(ResteFigure,AreteDessin,Placement).
-iter_figure([[Pf1,Pf2]|_],[Pd1,Pd2],[[Pf1,Pf2],[Pd1,Pd2]]):-  
-    distance(Pf1,Pf2,Df), distance(Pd1,Pd2,Dd), Df == Dd.
+test_placement_figure_dessin(_,_):-fail.
 
 
